2e48c24266b3e44c22efd06a6e38f3f291b8f421
Vitalii skus <vskus>
2023-03-29 11:05:48 +0300

    test netlify v7

--- netlify/functions/test-coverage.js
+++ netlify/functions/test-coverage.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const cors = require('cors');
-const { Configuration, OpenAIApi } = require("openai");
+import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     organization: process.env.ORGANIZATION,
@@ -12,29 +9,29 @@ const openai = new OpenAIApi(configuration);
 
 dotenv.config();
 
-app.use(cors());
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-
-app.post('/api/v1/test-coverage', async (req, res) => {
-    try {
-        const { content, rulesType } = req.body;
-
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: 'user', content: Buffer.from(content, "utf-8").toString() + rules[rulesType]}],
-    });
-
-        return res.send({content: extractCode(completion.data.choices[0].message.content)})
-    } catch (e) {
-        console.log('err', e)
-        return res.send({content: 'Oooops. Try one more time.'});
-    }
-});
+const rules = {
+    testCoverage: `
+        Can you cover this code with unit test?
+        Please, apply next rules:
+            1. Every expect statement be wrapped on "it".
+            2. Not allowed use multiple expects in one "it" block.
+            3. Use "Jest" library.
+            4. Feel free to make an assumption regarding frameworks.
+            5. For marcdown symbols use word \`\`\`javascript, not \`\`\`js. it's required to format code with Marcdown: \`\`\`javascript
+`,
+    simplifyCode: `
+        Can you simplify this code?
+        Please, apply next rules:
+            1. It should be easy to read.
+            2. Try not to use javascript methods that mutate state. Such as: push, pop, splice. Use pure methods.
+            3. Feel free to create smaller functions if needed.
+            4. Feel free to make an assumption regarding frameworks.
+            5. For marcdown symbols use word \`\`\`javascript, not \`\`\`js. it's required to format code with Marcdown: \`\`\`javascript
+            6. Use this styleguide for code: https://github.com/airbnb/javascript/blob/master/README.md  
+            7. Use approach from functional Programming in Javascript           
+            8. Move Magic numbers, string etc to constants. 
+`
+};
 
 const extractCode = text => {
     try {
@@ -42,7 +39,30 @@ const extractCode = text => {
     } catch (e) {
         return text;
     }
+};
 
-}
+const handler = async (event, context) => {
+    try {
+        const { content, rulesType } = JSON.parse(event.body);
 
-module.exports = app;
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: 'user', content: Buffer.from(content, "utf-8").toString() + ( rules[rulesType] as string)}],
+    });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                content: extractCode(completion.data.choices[0].message.content),
+            }),
+        };
+    } catch (e) {
+        console.log('err', e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({content: 'Oooops. Try one more time.'}),
+        };
+    }
+};
+
+export { handler };
